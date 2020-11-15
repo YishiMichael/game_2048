a6331b234e2595625d205e1a86ae5528327d4e17
Michael1075 <3375630562@qq.com>
2020-11-15 11:00:41 +0800

    Update files via changed

--- game_2048.py
+++ game_2048.py
@@ -46,13 +46,15 @@ class Main(object):
         self.shape = np.array(shape or self.DEFAULT_SHAPE)
         self.base = base or self.DEFAULT_BASE
         self.dim = self.shape.size
-        assert self.dim <= 12
+        assert self.dim <= 6
         self.size = np.prod(self.shape)
-        self.view_map = np.zeros(self.shape, dtype=np.int8)
+        self.view_map = np.zeros(self.size, dtype=np.int8)
         #self.buttonmap = []
         #self.style = None
         self.x_axis_units, self.y_axis_units = self.get_axis_units()
         self.canvas = self.set_canvas()
+        self.tiles = [0] * self.size
+        self.texts = [0] * self.size
         self.start_game()
 
         # score system
@@ -62,21 +64,6 @@ class Main(object):
 
     def unravel(self, index):
         return np.unravel_index(index, self.shape)
-
-    ## Really questionable ... if no function can make this
-    def get_tensor_val(self, tensor, indices):
-        return tensor.flatten()[self.ravel(indices)]
-
-    def set_tensor_val(self, tensor, indices, val):
-        temp = tensor.flatten()
-        temp[self.ravel(indices)] = val
-        return temp.reshape(tensor.shape)
-
-    def get_box_val(self, indices):
-        return self.get_tensor_val(self.view_map, indices)
-
-    def set_box_val(self, indices, val):
-        self.view_map = self.set_tensor_val(self.view_map, indices, val)
 
     def get_axis_units(self):
         # indices = (3, 6, 7, 2, 5, 4)
@@ -108,7 +95,8 @@ class Main(object):
         )[::-1]
         return x_axis_units, y_axis_units
 
-    def get_box_anchor_point(self, indices):
+    def get_box_anchor_point(self, index):
+        indices = self.unravel(index)
         return np.array([
             np.sum(indices[::2] * self.x_axis_units),
             np.sum(indices[1::2] * self.y_axis_units)
@@ -122,7 +110,7 @@ class Main(object):
 
     def set_canvas(self):
         self.root.title("Game of 2048")
-        full_size = self.get_box_anchor_point(self.shape) \
+        full_size = self.get_box_anchor_point(self.size - 1) \
             + self.BOX_SIDE_LENGTH + self.FRAME_WIDTH
         self.root.geometry("{0}x{1}".format(
             int(full_size[0]), int(full_size[1])
@@ -135,81 +123,91 @@ class Main(object):
         canvas.pack()
         return canvas
 
-    def draw_box(self, indices, val):
-        #self.view_map[indices] = val
-        box_anchor = self.get_box_anchor_point(indices)
+    def draw_background_box(self, index):
+        box_anchor = self.get_box_anchor_point(index)
+        x0, y0 = box_anchor
+        x1, y1 = box_anchor + self.BOX_SIDE_LENGTH
+        xc, yc = box_anchor + self.BOX_SIDE_LENGTH / 2
+        tile_color = self.safe_fetch(self.TILE_COLOR_MAP, 0)
+        text_color = self.safe_fetch(self.TEXT_COLOR, 0)
+        self.canvas.create_rectangle(
+            x0, y0, x1, y1, fill=tile_color
+        )
+
+    def draw_tile(self, index):
+        val = self.view_map[index]
+        assert val
+        box_anchor = self.get_box_anchor_point(index)
         x0, y0 = box_anchor
         x1, y1 = box_anchor + self.BOX_SIDE_LENGTH
         xc, yc = box_anchor + self.BOX_SIDE_LENGTH / 2
         tile_color = self.safe_fetch(self.TILE_COLOR_MAP, val)
-        self.canvas.create_rectangle(
-            x0, y0, x1, y1, fill=tile_color,
-            tag="box {0}".format(repr(indices))
+        text_color = self.safe_fetch(self.TEXT_COLOR, val)
+        self.tiles[index] = self.canvas.create_rectangle(
+            x0, y0, x1, y1, fill=tile_color
         )
-        if val:
-            text_color = self.safe_fetch(self.TEXT_COLOR, val)
-            self.canvas.create_text(
-                xc, yc, text=str(2 ** val), fill=text_color,
-                font=self.DISPLAY_FONT, tag="text {0}".format(repr(indices))
-            )
+        self.texts[index] = self.canvas.create_text(
+            xc, yc, text=str(self.base ** val), fill=text_color,
+            font=self.DISPLAY_FONT
+        )
 
-    def draw_tile(self, indices):
-        print(indices)
-        val = self.get_box_val(indices)
-        assert val
-        self.draw_box(indices, val)
+    def delete_tile(self, index):
+        self.canvas.delete(self.tiles[index])
+        self.canvas.delete(self.texts[index])
+        self.tiles[index] = 0
+        self.texts[index] = 0
 
-    def delete_tile(self, indices):
-        self.canvas.delete("box {0}".format(repr(indices)))
-        self.canvas.delete("text {0}".format(repr(indices)))
+    def appear_tile(self, index):
+        self.draw_tile(index)
 
-    def appear_tile(self, indices):
-        self.draw_tile(indices)
-
-    def move_tile(self, from_indices, to_indices, save=True):
-        self.delete_tile(from_indices)
-        if save:
-            self.draw_tile(to_indices)
+    def move_tiles(self, link_data):
+        for from_index in link_data.from_index:
+            self.delete_tile(from_index)
+        for to_index in set(link_data.to_index):
+            self.draw_tile(to_index)
 
     def shift(self, axis, opposite):
-        self.view_map, *movements, score = logic.push_tiles_using_numpy(
-            self.view_map, self.base, axis=axis, opposite=opposite
+        nvm, link_data, score = logic.push_tiles_using_numpy(
+            self.view_map.reshape(self.shape), self.base, axis=axis, opposite=opposite
         )
-        movements = list(zip(*movements))
-        return movements
+        self.view_map = np.fromiter(nvm.flat, np.int8)
+        return link_data
 
     def get_new_num(self):
         choices = np.flatnonzero(self.view_map == 0)
         assert choices.size
         new_num_index = np.random.choice(choices)
-        return self.unravel(new_num_index), 1
+        return new_num_index, 1
 
-    def action(self, event, axis, opposite=False):
+    def action(self, event):
+        char = event.char
+        try:
+            axis = "lkhgdsjiftaw".index(char) % 6  # TODO, shape == (2, 2, 2, 2, 2, 2)
+        except ValueError:
+            return False
+        if axis >= self.dim:
+            return False
+        opposite = char in "lkhgds"
         old_map = self.view_map
-        movements = self.shift(axis, opposite)
+        link_data = self.shift(axis, opposite)
         if np.array_equal(old_map, self.view_map):
             return False
-        for from_indices, to_indices, save in movements:
-            self.move_tile(from_indices, to_indices, save)
-        new_num_indices, num = self.get_new_num()
-        self.set_box_val(new_num_indices, num)
-        self.appear_tile(new_num_indices)
+        self.move_tiles(link_data)
+        new_num_index, num = self.get_new_num()
+        self.view_map[new_num_index] = num
+        self.appear_tile(new_num_index)
         return True
 
     def start_game(self):
         for index in range(self.size):
-            self.draw_box(self.unravel(index), 0)
+            self.draw_background_box(index)
         init_location = np.random.choice(
-            np.arange(self.size), 4, replace=False ## init
+            np.arange(self.size), 2, replace=False ## init
         )
         for index in init_location:
-            self.set_box_val(self.unravel(index), 1)
-            self.appear_tile(self.unravel(index))
-        
-        for i in range(self.dim):
-            self.root.bind("watfij"[::-1][i], lambda e: self.action(e, i, False))
-            self.root.bind("sdghkl"[::-1][i], lambda e: self.action(e, i, True))
-
+            self.view_map[index] = 1
+            self.appear_tile(index)
+        self.root.bind("<Key>", self.action)
         self.root.mainloop()
         
 
--- logic.py
+++ logic.py
@@ -2,7 +2,14 @@ import numpy as np
 import time
 
 
-__all__ = ["push_tiles_using_numpy", "push_tiles_using_loops"]
+__all__ = ["push_tiles_using_numpy", "push_tiles_using_loops", "push_tiles_in_vectorized_map"]
+
+
+link_dtype = np.dtype([("from_index", np.int8), ("to_index", np.int8), ("whether_merge", np.bool_)])
+
+
+def make_link_data(from_indices, to_indices, whether_merge):
+    return np.core.records.fromarrays((from_indices, to_indices, whether_merge), dtype=link_dtype)
 
 
 def add_opposite_direction_attribute(accumulate_func):
@@ -51,8 +58,14 @@ def stretch(a, stretch_size, *, axis, fill=0, at_tail=True):
     return np.concatenate(pair, axis=axis)
 
 
-def all_equal(a, axis=0):
+def all_equal(a, *, axis=0):
     return a.max(axis=axis) == a.min(axis=axis)
+
+
+def ravel_argwhere_along_axis(a, *, axis):
+    template_indices = np.moveaxis(np.arange(a.size).reshape(a.shape), axis, -1)
+    result = np.where(np.moveaxis(a, axis, -1), template_indices, np.full_like(template_indices, fill_value=-1))
+    return result[result != -1]
 
 
 def move_tiles(vm, *, axis, opposite):
@@ -78,8 +91,8 @@ def merge_tiles(vm, base, *, axis, opposite):
     preserved_boxes_mask = separate_decrement(merged_tiles_mask * base, axis=axis, opposite=opposite) <= 0
     nvm = np.select((merged_tiles_mask, preserved_boxes_mask), (vm + 1, vm))
     repeat_times = np.select((merged_tiles_mask, nvm != 0), (np.full_like(vm, fill_value=base), np.ones_like(vm)))
-    repeat_times = repeat_times[repeat_times != 0]
-    score = (base ** (nvm * merged_tiles_mask)).sum()
+    repeat_times = repeat_times.flat[ravel_argwhere_along_axis(repeat_times, axis=axis)]
+    score = (base ** nvm[merged_tiles_mask != 0]).sum()
     return nvm, repeat_times, score
 
 
@@ -88,17 +101,20 @@ def push_tiles_using_numpy(vm, base, *, axis, opposite):
     vm1 = move_tiles(vm, axis=axis, opposite=opposite)
     vm2, repeat_times, score = merge_tiles(vm1, base, axis=axis, opposite=opposite)
     nvm = move_tiles(vm2, axis=axis, opposite=opposite)
-    from_indices = np.argwhere(vm)
-    to_indices = np.argwhere(nvm).repeat(repeat_times, axis=0)
-    merge = (repeat_times != 1).repeat(repeat_times)
-    return nvm, from_indices, to_indices, merge, score
+
+    from_indices = ravel_argwhere_along_axis(vm, axis=axis)
+    to_indices = ravel_argwhere_along_axis(nvm, axis=axis).repeat(repeat_times)
+    whether_merge = (repeat_times != 1).repeat(repeat_times)
+    link_data = make_link_data(from_indices, to_indices, whether_merge)
+    link_data.sort(order="from_index")
+    return nvm, link_data, score
 
 
 def push_tiles_along_axis_1(vm, base):
     nvm = np.zeros_like(vm)
     from_indices = []
     to_indices = []
-    merge = []
+    whether_merge = []
     temp_indices = []
     score = 0
     for i, line in enumerate(vm):
@@ -118,7 +134,7 @@ def push_tiles_along_axis_1(vm, base):
                     nvm[i, nvm_p] = val + 1
                     line_from_indices.extend(temp_indices)
                     line_to_indices.extend([nvm_p] * count)
-                    merge.extend([True] * count)
+                    whether_merge.extend([True] * count)
                     score += base ** (val + 1)
                     nvm_p += 1
                     hold = 0
@@ -128,7 +144,7 @@ def push_tiles_along_axis_1(vm, base):
                 nvm[i, nvm_p:nvm_p + count] = hold
                 line_from_indices.extend(temp_indices)
                 line_to_indices.extend(list(range(nvm_p, nvm_p + count)))
-                merge.extend([False] * count)
+                whether_merge.extend([False] * count)
                 nvm_p += count
                 hold = val
                 count = 1
@@ -136,7 +152,7 @@ def push_tiles_along_axis_1(vm, base):
         nvm[i, nvm_p:nvm_p + count] = hold
         line_from_indices.extend(temp_indices)
         line_to_indices.extend(list(range(nvm_p, nvm_p + count)))
-        merge.extend([False] * count)
+        whether_merge.extend([False] * count)
         from_indices.append(line_from_indices)
         to_indices.append(line_to_indices)
 
@@ -145,10 +161,13 @@ def push_tiles_along_axis_1(vm, base):
             for j in line:
                 yield i
                 yield j
+
+    def compound_list_to_array(compound_list):
+        return np.fromiter(generator_pair_from_compound_list(compound_list), dtype=np.int8).reshape((-1, 2))
     
-    from_indices = np.fromiter(generator_pair_from_compound_list(from_indices), dtype=np.int8).reshape((-1, 2))
-    to_indices = np.fromiter(generator_pair_from_compound_list(to_indices), dtype=np.int8).reshape((-1, 2))
-    return nvm, from_indices, to_indices, merge, score
+    from_indices = compound_list_to_array(from_indices)
+    to_indices = compound_list_to_array(to_indices)
+    return nvm, from_indices, to_indices, whether_merge, score
 
 
 def push_tiles_using_loops(vm, base, *, axis, opposite):
@@ -156,50 +175,146 @@ def push_tiles_using_loops(vm, base, *, axis, opposite):
     if opposite:
         vm = np.flip(vm, axis=axis)
     original_shape = vm.shape
-    vm = np.moveaxis(vm, axis, -1)
+    vm = np.swapaxes(vm, axis, -1)
     temp_shape = vm.shape
     vm = vm.reshape((-1, original_shape[axis]))
-    nvm, from_indices, to_indices, merge, score = push_tiles_along_axis_1(vm, base)
+    nvm, from_indices, to_indices, whether_merge, score = push_tiles_along_axis_1(vm, base)
     nvm = nvm.reshape(temp_shape)
-    nvm = np.moveaxis(nvm, -1, axis)
+    nvm = np.swapaxes(nvm, -1, axis)
     if opposite:
         nvm = np.flip(nvm, axis=axis)
 
-    def recover_axes(indices):
+    def recover_axes_and_flatten(indices):
         recovered_indices = np.array(np.unravel_index(np.ravel_multi_index(indices.T, vm.shape), temp_shape))
         recovered_indices[[axis, -1]] = recovered_indices[[-1, axis]]
         if opposite:
             recovered_indices[axis] = original_shape[axis] - 1 - recovered_indices[axis]
-        return recovered_indices.T
+        return np.ravel_multi_index(recovered_indices, original_shape)
 
-    from_indices = recover_axes(from_indices)
-    to_indices = recover_axes(to_indices)
-    return nvm, from_indices, to_indices, merge, score
+    from_indices = recover_axes_and_flatten(from_indices)
+    to_indices = recover_axes_and_flatten(to_indices)
+    link_data = make_link_data(from_indices, to_indices, whether_merge)
+    link_data.sort(order="from_index")
+    return nvm, link_data, score
+
+
+def push_tiles_in_vectorized_map(vm, shape, base, *, axis, opposite):
+    assert base > 1
+    nvm = np.zeros_like(vm)
+    from_indices = []
+    to_indices = []
+    whether_merge = []
+    score = 0
+    minor_step = np.prod(shape[axis + 1:], dtype=np.int8)
+    major_step = minor_step * shape[axis]
+    if not opposite:
+        for i0 in range(0, vm.size, major_step):
+            for i1 in range(i0, i0 + minor_step):
+                nvm_p = i1
+                hold = 0
+                count = 0
+                temp_indices = []
+                for index in range(i1, i1 + major_step, minor_step):
+                    val = vm[index]
+                    if val == 0:
+                        pass
+                    elif val == hold:
+                        count += 1
+                        temp_indices.append(index)
+                        if count == base:
+                            nvm[nvm_p] = val + 1
+                            from_indices.extend(temp_indices)
+                            to_indices.extend([nvm_p] * count)
+                            whether_merge.extend([True] * count)
+                            score += base ** (val + 1)
+                            nvm_p += minor_step
+                            hold = 0
+                            count = 0
+                            temp_indices = []
+                    else:
+                        nvm[nvm_p:nvm_p + count * minor_step:minor_step] = hold
+                        from_indices.extend(temp_indices)
+                        to_indices.extend(list(range(nvm_p, nvm_p + count * minor_step, minor_step)))
+                        whether_merge.extend([False] * count)
+                        nvm_p += count * minor_step
+                        hold = val
+                        count = 1
+                        temp_indices = [index]
+                nvm[nvm_p:nvm_p + count * minor_step:minor_step] = hold
+                from_indices.extend(temp_indices)
+                to_indices.extend(list(range(nvm_p, nvm_p + count * minor_step, minor_step)))
+                whether_merge.extend([False] * count)
+    else:
+        for i0 in range(0, vm.size, major_step):
+            for i1 in range(i0, i0 + minor_step):
+                nvm_p = i1 + major_step - minor_step
+                hold = 0
+                count = 0
+                temp_indices = []
+                for index in range(i1 + major_step - minor_step, i1 - minor_step, -minor_step):
+                    val = vm[index]
+                    if val == 0:
+                        pass
+                    elif val == hold:
+                        count += 1
+                        temp_indices.append(index)
+                        if count == base:
+                            nvm[nvm_p] = val + 1
+                            from_indices.extend(temp_indices)
+                            to_indices.extend([nvm_p] * count)
+                            whether_merge.extend([True] * count)
+                            score += base ** (val + 1)
+                            nvm_p -= minor_step
+                            hold = 0
+                            count = 0
+                            temp_indices = []
+                    else:
+                        nvm[nvm_p:nvm_p - count * minor_step:-minor_step] = hold
+                        from_indices.extend(temp_indices)
+                        to_indices.extend(list(range(nvm_p, nvm_p - count * minor_step, -minor_step)))
+                        whether_merge.extend([False] * count)
+                        nvm_p -= count * minor_step
+                        hold = val
+                        count = 1
+                        temp_indices = [index]
+                if nvm_p - count * minor_step < 0:
+                    nvm[nvm_p::-minor_step] = hold
+                else:
+                    nvm[nvm_p:nvm_p - count * minor_step:-minor_step] = hold
+                from_indices.extend(temp_indices)
+                to_indices.extend(list(range(nvm_p, nvm_p - count * minor_step, -minor_step)))
+                whether_merge.extend([False] * count)
+    link_data = make_link_data(from_indices, to_indices, whether_merge)
+    link_data.sort(order="from_index")
+    return nvm, link_data, score
 
 
 def test_equal():
     test_cases = 100
     test_shape = (5, 4, 3, 4)
     test_vm = np.random.randint(4, size=(test_cases, *test_shape))
-    #vm = np.array([[3, 2, 2, 1], [0, 2, 2, 3], [1, 1, 2, 1], [0, 0, 2, 0]])
     test_base = np.random.randint(2, 4, size=test_cases)
     test_axis = np.random.randint(len(test_shape), size=test_cases)
     test_opposite = np.random.randint(2, size=test_cases).astype(np.bool_)
+    #test_vm = [np.array([[3, 2, 2, 1], [0, 2, 2, 3], [1, 1, 2, 1], [0, 0, 2, 0]])]
+    #test_base = [2]
+    #test_axis = [0]
+    #test_opposite = [True]
     for vm, base, axis, opposite in zip(test_vm, test_base, test_axis, test_opposite):
-        nvm0, from_indices0, to_indices0, merge0, score0 = push_tiles_using_numpy(vm, base, axis=axis, opposite=opposite)
-        nvm1, from_indices1, to_indices1, merge1, score1 = push_tiles_using_numpy(vm, base, axis=axis, opposite=opposite)
+        nvm0, link_data0, score0 = push_tiles_using_numpy(vm, base, axis=axis, opposite=opposite)
+        nvm1, link_data1, score1 = push_tiles_using_loops(vm, base, axis=axis, opposite=opposite)
+        nvm2, link_data2, score2 = push_tiles_in_vectorized_map(vm.reshape(-1), vm.shape, base, axis=axis, opposite=opposite)
         assert np.array_equal(nvm0, nvm1)
-        assert np.array_equal(from_indices0, from_indices1)
-        assert np.array_equal(to_indices0, to_indices1)
-        assert np.array_equal(merge0, merge1)
-        assert score0 == score1
+        assert np.array_equal(nvm0, nvm2.reshape(vm.shape))
+        assert np.array_equal(link_data0, link_data1)
+        assert np.array_equal(link_data0, link_data2)
+        assert score0 == score1 == score2
 
 
 def test_time():
     test_cases = 1000
     test_shape = (5, 4, 3, 4)
     test_vm = np.random.randint(4, size=(test_cases, *test_shape))
-    #vm = np.array([[3, 2, 2, 1], [0, 2, 2, 3], [1, 1, 2, 1], [0, 0, 2, 0]])
     test_base = np.random.randint(2, 4, size=test_cases)
     test_axis = np.random.randint(len(test_shape), size=test_cases)
     test_opposite = np.random.randint(2, size=test_cases).astype(np.bool_)
@@ -211,6 +326,11 @@ def test_time():
     begin_time = time.time()
     for vm, base, axis, opposite in zip(test_vm, test_base, test_axis, test_opposite):
         _ = push_tiles_using_loops(vm, base, axis=axis, opposite=opposite)
+    end_time = time.time()
+    print(end_time - begin_time)
+    begin_time = time.time()
+    for vm, base, axis, opposite in zip(test_vm, test_base, test_axis, test_opposite):
+        _ = push_tiles_in_vectorized_map(vm.reshape(-1), vm.shape, base, axis=axis, opposite=opposite)
     end_time = time.time()
     print(end_time - begin_time)
 
